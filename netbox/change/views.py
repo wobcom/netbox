c6ac9358bd0b84f18f3a9fa507c81d791aea3be8
hellerve <veit@veitheller.de>
2018-10-15 16:10:02 +0200

    change: proper yaml result

--- netbox/change/views.py
+++ netbox/change/views.py
@@ -1,6 +1,8 @@
 from datetime import datetime
+import yaml
 
 from django.contrib.auth.decorators import login_required
+from django.db import models
 from django.shortcuts import redirect, render
 from django.utils.decorators import method_decorator
 from django.views.generic import View
@@ -21,10 +23,32 @@ class ToggleView(View):
         # the standard deserialization from datetime
         change_time = datetime.strptime(request.session["change_started"],
                                         "%Y-%m-%d %H:%M:%S.%f")
-        changes = ChangedField.objects.filter(user=request.user,
-                                              time__gt=change_time)
+        change_objs = ChangedField.objects.filter(user=request.user,
+                                                  time__gt=change_time)
+
+        changes = []
+
+        for change in change_objs:
+            changed_object = {}
+
+            for field in change.changed_object._meta.fields:
+                if field.__class__ == models.ForeignKey:
+                    continue
+
+                fname = field.name
+                changed_object[fname] = getattr(change.changed_object, fname)
+
+
+            changes.append({
+                "field": change.field,
+                "old_value": change.old_value,
+                "new_value": change.new_value,
+                "type": str(change.changed_object_type),
+                "changed_object": changed_object
+            })
 
         # for now just render the result
         return render(request, 'change/list.html', {
-            'changes': changes,
+            'changes': yaml.dump(changes,explicit_start=True,
+                                 default_flow_style=False),
         })
