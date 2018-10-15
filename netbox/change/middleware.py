daddcc09272247f9f1167ebf5c59751126b46f9b
hellerve <veit@veitheller.de>
2018-10-15 15:26:56 +0200

    change: refactor middleware

--- netbox/change/middleware.py
+++ netbox/change/middleware.py
@@ -5,59 +5,55 @@ from extras.models import ObjectChange
 
 from .models import ChangedField
 
-# TODO: these global variables shouldnt be necessary. Why do Django signals not
-# trigger on non-global functions?
-user = None
-worked_on = False
-
 blacklist = [
     ChangedField,
     ObjectChange,
     Session
 ]
 
-def before_save_internal(sender, instance, **kwargs):
-    if sender in blacklist:
-        return
-    try:
-        old_instance = sender.objects.get(id=instance.id)
-    except sender.DoesNotExist:
-        return
-    for field in sender._meta.fields:
-        old_value = getattr(old_instance, field.name)
-        new_value = getattr(instance, field.name)
-
-        if new_value == old_value:
-            continue
-
-        ChangedField(
-            changed_object=instance,
-            field = field.name,
-            old_value = getattr(old_instance, field.name),
-            new_value = getattr(instance, field.name),
-            user = user,
-        ).save()
-    worked_on = True
-
-
-def after_save_internal(sender, instance, **kwargs):
-    if sender in blacklist or worked_on:
-        return
-    for field in sender._meta.fields:
-        ChangedField(
-            changed_object=instance,
-            field = field.name,
-            old_value = None,
-            new_value = getattr(instance, field.name),
-            user = user,
-        ).save()
-
 
 def before_save(request):
-    global user
-    user = request.user
-    pre_save.connect(before_save_internal)
-    post_save.connect(after_save_internal)
+    worked_on = False
+
+    def before_save_internal(sender, instance, **kwargs):
+        # TODO: urgh!
+        nonlocal worked_on
+        if sender in blacklist:
+            return
+        try:
+            old_instance = sender.objects.get(id=instance.id)
+        except sender.DoesNotExist:
+            return
+        for field in sender._meta.fields:
+            old_value = getattr(old_instance, field.name)
+            new_value = getattr(instance, field.name)
+
+            if new_value == old_value:
+                continue
+
+            ChangedField(
+                changed_object=instance,
+                field=field.name,
+                old_value=getattr(old_instance, field.name),
+                new_value=getattr(instance, field.name),
+                user=request.user,
+            ).save()
+        worked_on = True
+
+    def after_save_internal(sender, instance, **kwargs):
+        if sender in blacklist or worked_on:
+            return
+        for field in sender._meta.fields:
+            ChangedField(
+                changed_object=instance,
+                field=field.name,
+                old_value=None,
+                new_value=getattr(instance, field.name),
+                user=request.user,
+            ).save()
+
+    pre_save.connect(before_save_internal, weak=False, dispatch_uid="chgfield")
+    post_save.connect(after_save_internal, weak=False, dispatch_uid="chgfield")
 
 
 class FieldChangeMiddleware(object):
@@ -65,7 +61,6 @@ class FieldChangeMiddleware(object):
         self.get_response = get_response
 
     def __call__(self, request):
-        worked_on = False
         if request.session.get("in_change", False):
             before_save(request)
 
