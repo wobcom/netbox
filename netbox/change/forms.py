65556026f0d30bd827f379026e8dc420b10ca4c6
hellerve <veit@veitheller.de>
2018-10-22 08:37:46 -0400

    change: add comments to change mixin

--- netbox/change/forms.py
+++ netbox/change/forms.py
@@ -11,13 +11,16 @@ class ChangeMixin(forms.BaseForm):
     def __init__(self, *args, **kwargs):
         super(ChangeMixin, self).__init__(*args, **kwargs)
 
+        # Get the change info
         in_change = kwargs.pop('in_change', False)
         change_started = kwargs.pop('change_started', None)
         user = kwargs.pop('user', None)
 
+        # if were not in a change, we can ignore this mixin
         if not in_change:
             return
 
+        # get the changes we need to apply
         change_started = datetime.strptime(change_started,
                                           "%Y-%m-%d %H:%M:%S.%f")
 
