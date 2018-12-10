261129b7230a1838e8834f0308902fbd6ae8dfa1
Carl Fabian Lüpke <fabian.luepke@wobcom.de>
2018-12-10 20:48:17 +0100

    Added a gitlab-runner and a script to automagically register it.

---
+++ scripts/register_runner.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+source .env
+
+echo "Obtaining shared gitlab runner token from gitlab"
+GITLAB_SHARED_RUNNER_TOKEN=$(docker exec -it $(docker inspect --format="{{.Id}}" "netbox_gitlab") /opt/gitlab/bin/gitlab-psql -qtAX -d "gitlabhq_production" -c "SELECT runners_registration_token FROM application_settings ORDER BY id
+DESC LIMIT 1")
+
+echo "Registering gitlab runner with gitlab"
+
+docker exec -it $(docker inspect --format="{{.Id}}" "netbox_gitlab_runner") /usr/bin/gitlab-runner register \
+      --non-interactive \
+      --url "$GITLAB_URL" \
+      --registration-token "$GITLAB_SHARED_RUNNER_TOKEN" \
+      --executor "docker" \
+      --docker-image "alpine:latest" \
+      --description "gitlab-runner" \
+      --tag-list "docker,changes" \
+      --run-untagged \
+      --locked="false"
