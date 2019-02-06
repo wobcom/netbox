6f7bad751bab5fc89c4eed317fbc753796911143
hellerve <veit@veitheller.de>
2019-02-06 10:27:40 +0100

    dcim: prettify is_lag with constants

--- netbox/dcim/models.py
+++ netbox/dcim/models.py
@@ -2148,7 +2148,7 @@ class Interface(CableTermination, ComponentModel):
 
     @property
     def is_lag(self):
-        return self.form_factor in [IFACE_FF_LAG, IFACE_FF_BRIDGE]
+        return self.form_factor in AGGREGATABLE_IFACE_TYPES
 
     @property
     def is_bridge(self):
