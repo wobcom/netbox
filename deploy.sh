b175a43364ae974fa562097770c64b687ee0397b
Thomas Karmann <thomas.karmann@port-zero.com>
2019-02-13 17:12:09 +0100

    * make db migrations after git pull

--- deploy.sh
+++ deploy.sh
@@ -3,24 +3,20 @@ set -e
 
 cd /opt/netbox/netbox
 
+# checkout repository
 git pull http://gitlab+deploy-token-3:UhhtBKPa9FL-aBKUe-Ax@gitlab.service.wobcom.de/infrastructure/netbox.git@changes
-#git pull https://gitlab-ci-token:<YOUR_TOKEN>@<GITLAB_INSTANCE_URL>/<USERNAME_OR_GROUPNAME/<REPO_NAME>.git
-#http://gitlab.service.wobcom.de/infrastructure/netbox.git
+
+## do DB migrations
+venv/bin/python netbox/manage.py migrate
 
 supervisorctl restart all 
 
 #docker login -u mimir_mvp -p R-SUsvUJBQinAzCH6gPp registry.gitlab.com
-
 #docker-compose down --remove-orphans || /bin/true
-
 # clean 
 #docker system prune -a -f
-
 #docker rm mimir || /bin/true
-
 #docker pull registry.gitlab.com/wobcom/mimir:latest
-
 #docker-compose up
 #docker run -p 80:80 -p 443:443 --name mimir -d registry.gitlab.com/wobcom/mimir:latest
-#   docker-compose up -d 
-
+#docker-compose up -d 
