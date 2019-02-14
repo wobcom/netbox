197ab1120dc4b576e9205ef1f61e9afd267f870a
Thomas Karmann <thomas.karmann@port-zero.com>
2019-02-14 11:43:52 +0100

    * deploy.sh now uses ssh-agent to git pull

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,10 @@ set -e
 cd /opt/netbox/netbox
 
 # method proposed by https://stackoverflow.com/a/44702187
-git config --global credential.helper store --file=/var/tmp/autodeploy/.git-credentials
+# git config --global credential.helper store --file=/var/tmp/autodeploy/.git-credentials
+
+eval $(ssh-agent -s)   
+ssh-add ~/.ssh/ci\@gitlab.service.wobcom.de
 
 # checkout repository
 # @branch not working
