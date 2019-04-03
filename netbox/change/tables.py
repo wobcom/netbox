653d4a8e781903e5199d5042bca47794aaa3ab7e
hellerve <veit@veitheller.de>
2019-04-03 11:24:15 +0200

    change: disable merge request recreation when change was already implemented

--- netbox/change/tables.py
+++ netbox/change/tables.py
@@ -1,12 +1,12 @@
 import django_tables2 as tables
 
 from utilities.tables import BaseTable
-from .models import ChangeSet
+from .models import ChangeSet, IMPLEMENTED
 
 
 RECREATE_MR = """
-<a href="{% url "change:change_mr" pk=record.pk %}" class="btn btn-primary">Recreate Merge Request</a>
-"""
+<a href="{{% url "change:change_mr" pk=record.pk %}}" class="btn btn-primary {{% if record.status == {} %}}disabled{{% endif %}}">Recreate Merge Request</a>
+""".format(IMPLEMENTED)
 RECREATE_TOPDESK = """
 <a href="{% url "change:change_topdesk" pk=record.pk %}" class="btn btn-primary">Recreate TOPdesk Ticket</a>
 """
