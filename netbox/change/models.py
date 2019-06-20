201acbe858bd7ccb95318450b46d3a0317a6998a
Julian Jacobi <julian.jacobi@wobcom.de>
2019-06-20 09:09:11 +0200

    backported bgp migration

--- netbox/change/models.py
+++ netbox/change/models.py
@@ -22,6 +22,7 @@ from virtualization.models import VirtualMachine
 
 from change.utilities import Markdownify
 from dcim.constants import *
+from configuration.models import RouteMap, BGPCommunity, BGPCommunityList
 
 DEVICE_ROLE_TOPOLOGY_WHITELIST = ['leaf', 'spine', 'superspine']
 
@@ -249,6 +250,75 @@ class ChangeSet(models.Model):
             res[0]['extra_fields'] = self.yamlify_extra_fields(interface)
         return res
 
+    def yamlify_bgp_neighbor(self, neighbor):
+        res = {
+            'description': neighbor.description,
+            'routemap_in': neighbor.routemap_in.name if neighbor.routemap_in is not None else None,
+            'routemap_out': neighbor.routemap_out.name if neighbor.routemap_out is not None else None,
+            'remote_as': neighbor.remote_asn,
+            'status': neighbor.status,
+            'next_hop_self': neighbor.next_hop_self,
+            'remove_private_as': neighbor.remove_private_as,
+            'send_community': neighbor.send_community,
+            'soft_reconfiguration': neighbor.soft_reconfiguration,
+        }
+        if neighbor.neighbor_type == 'internal':
+            res['remote_ip'] = neighbor.internal_neighbor_ip.address
+        elif neighbor.neighbor_type == 'external':
+            res['remote_ip'] = neighbor.external_neighbor
+        if neighbor.source_interface is not None:
+            res['source_interface'] = neighbor.source_interface.name
+        return res
+
+    def yamlify_bgp(self, device):
+        res = {}
+
+        asns = device.bgpdeviceasn_set.all()
+
+        for asn in asns:
+            res[asn.asn.asn] = {}
+            res[asn.asn.asn]['exposed_networks'] = [str(p) for p in asn.get_exposed_prefixes()]
+            res[asn.asn.asn]['redistribute_local'] = asn.redistribute_connected
+            res[asn.asn.asn]['neighbors'] = []
+            neighbors = asn.neighbors.all()
+            for neighbor in neighbors:
+                res[asn.asn.asn]['neighbors'].append(self.yamlify_bgp_neighbor(neighbor))
+
+        return res
+
+    def yamlify_routemaps(self, device):
+        routemaps = RouteMap.objects.filter(models.Q(sessions_in__deviceasn__device=device) |
+                                            models.Q(sessions_out__deviceasn__device=device))
+
+        communities = BGPCommunity.objects.all()
+        res = []
+
+        for routemap in routemaps:
+            res.append({
+                'name': routemap.name,
+                'configuration': yaml.load(routemap.configuration.format(**{
+                    c.name: c.community for c in communities
+                }), Loader=yaml.SafeLoader),
+            })
+
+        return res
+
+    def yamlify_community_lists(self):
+        community_lists = BGPCommunityList.objects.all()
+        return [{
+            'name': lst.name,
+            'permit': [
+                member.community.community
+                for member
+                in lst.bgpcommunitylistmember_set.filter(type='permit')
+            ],
+            'deny': [
+                member.community.community
+                for member
+                in lst.bgpcommunitylistmember_set.filter(type='deny')
+            ],
+        } for lst in community_lists]
+
     def yamlify_device(self, device):
         res = {
             'type': self.yamlify_device_type(device.device_type),
@@ -261,6 +331,9 @@ class ChangeSet(models.Model):
             'tags': list(device.tags.names()),
             'interfaces': [],
             'primary_ip4': self.yamlify_ip_address(device.primary_ip4) if device.primary_ip4 else None,
+            'bgp': self.yamlify_bgp(device),
+            'routemaps': self.yamlify_routemaps(device),
+            'community-lists': self.yamlify_community_lists(),
             **self.yamlify_extra_fields(device)
         }
         for interface in device.interfaces.all():
