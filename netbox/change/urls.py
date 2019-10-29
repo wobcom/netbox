03aaa4aa3defa7b8f5c6657df6c997d454a5595e
hellerve <veit@veitheller.de>
2019-10-29 14:48:20 +0100

    change: graceful exit on gitlab error (references network-production#64)

--- netbox/change/urls.py
+++ netbox/change/urls.py
@@ -1,17 +1,16 @@
 from __future__ import unicode_literals
 
-from django.conf.urls import url
+from django.urls import path
 
 from . import views
 
 app_name = 'change'
 urlpatterns = [
-    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
-    url(r'^toggle/$', views.ToggleView.as_view(), name='toggle'),
-    url(r'^form/$', views.ChangeFormView.as_view(), name='form'),
-    url(r'^(?P<pk>\d+)/mr/$', views.MRView.as_view(), name='mr'),
-    url(r'^(?P<pk>\d+)/accept/$', views.AcceptView.as_view(), name='accept'),
-    url(r'^(?P<pk>\d+)/reject/$', views.RejectView.as_view(), name='reject'),
-    url(r'^(?P<pk>\d+)/reactivate/$', views.ReactivateView.as_view(),
+    path(r'<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path(r'toggle/', views.ToggleView.as_view(), name='toggle'),
+    path(r'form/', views.ChangeFormView.as_view(), name='form'),
+    path(r'<int:pk>/accept/', views.AcceptView.as_view(), name='accept'),
+    path(r'<int:pk>/reject/', views.RejectView.as_view(), name='reject'),
+    path(r'<int:pk>/reactivate/', views.ReactivateView.as_view(),
         name='reactivate'),
 ]
