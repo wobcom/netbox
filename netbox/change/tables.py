c2b550c478ce860f691f131ba6972d9a17990707
Julian Jacobi <julian.jacobi@wobcom.de>
2020-01-21 09:50:04 +0100

    fix profile changes list
    * remove recreate merge request button
    * remove reopen change button
    * add sorting by id
    * add updated column

--- netbox/change/tables.py
+++ netbox/change/tables.py
@@ -1,24 +1,10 @@
 import django_tables2 as tables
 
 from utilities.tables import BaseTable
-from .models import ChangeSet, IMPLEMENTED, DRAFT
+from .models import ChangeSet
 
 
-MR_LINK = """
-{% if record.mr_location %}
-    <a href="{{ record.mr_location }}">Merge Request</a>
-{% else %}
-    —
-{% endif %}
-"""
-
-RECREATE_MR = """
-<a href="{{% url "change:finalize" pk=record.pk %}}?recreate=true&redirect=change:accept" class="btn btn-primary {{% if record.status == {} %}}disabled{{% endif %}}"><i class="fa fa-play"></i></a>
-""".format(IMPLEMENTED)
-
-REACTIVATE = """
-<a href="{{% url "change:reactivate" pk=record.pk %}}" class="btn btn-primary {{% if record.status != {} %}}disabled{{% endif %}}"><i class="fa fa-play"></i></a>
-""".format(DRAFT)
+UPDATED = "{{record.updated | timesince }}"
 
 
 class ChangeTable(BaseTable):
@@ -26,21 +12,7 @@ class ChangeTable(BaseTable):
     status = tables.Column()
     changedfield_count = tables.Column(verbose_name='Changed Fields')
     changedobject_count = tables.Column(verbose_name='Changed Objects')
-    changedobject_count = tables.TemplateColumn(
-        verbose_name='Merge Request',
-        template_code=MR_LINK,
-        orderable=False
-    )
-    create_mr = tables.TemplateColumn(
-        verbose_name='Recreate MR',
-        template_code=RECREATE_MR,
-        orderable=False
-    )
-    reactivate = tables.TemplateColumn(
-        verbose_name='Reactivate',
-        template_code=REACTIVATE,
-        orderable=False
-    )
+    updated = tables.TemplateColumn(template_code=UPDATED, verbose_name='Updated')
 
     class Meta(BaseTable.Meta):
         model = ChangeSet
@@ -49,6 +21,5 @@ class ChangeTable(BaseTable):
             'status',
             'changedfield_count',
             'changedobject_count',
-            'create_mr',
-            'reactivate',
+            'updated',
         )
