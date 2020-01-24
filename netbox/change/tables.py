8e68a59780fa9286af680fd387e2fe898f7f05e2
Julian Jacobi <julian.jacobi@wobcom.de>
2020-01-24 09:54:38 +0100

    add provisions overview
    * new entry in provisions dropdown menu
    * provisions table and view

--- netbox/change/tables.py
+++ netbox/change/tables.py
@@ -1,10 +1,14 @@
 import django_tables2 as tables
 
 from utilities.tables import BaseTable
-from .models import ChangeSet
+from .models import ChangeSet, ProvisionSet
 
 
 UPDATED = "{{record.updated | timesince }}"
+
+PROVISION_UPDATED = '<span title="{{ record.updated }}">{{ record.updated | timesince }}</span>'
+
+PROVISION_CREATED = '<span title="{{ record.created }}">{{ record.created | timesince }}</span>'
 
 
 class ChangeTable(BaseTable):
@@ -22,4 +26,20 @@ class ChangeTable(BaseTable):
             'changedfield_count',
             'changedobject_count',
             'updated',
+        )
+
+
+class ProvisionTable(BaseTable):
+    pk = tables.LinkColumn('change:provisions', verbose_name="ID")
+    user = tables.Column(verbose_name='Creator')
+    updated = tables.TemplateColumn(template_code=PROVISION_UPDATED)
+    created = tables.TemplateColumn(template_code=PROVISION_CREATED)
+
+    class Meta(BaseTable.Meta):
+        model = ProvisionSet
+        fields = (
+            'pk',
+            'user',
+            'updated',
+            'created',
         )
--- netbox/change/urls.py
+++ netbox/change/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path(r'form/', views.ChangeFormView.as_view(), name='form'),
     path(r'end_change/', views.EndChangeView.as_view(), name='end_change'),
     path(r'deploy/', views.DeployView.as_view(), name='deploy'),
-    path(r'provisions/<int:pk>/terminate', views.TerminateView.as_view(), name='terminate_provision'),
+
+    path(r'provisions/', views.ProvisionsView.as_view(), name='provisions'),
 ]
