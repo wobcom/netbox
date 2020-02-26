9d47730bcc9709e71b4a8160181a5a56c1ccad7d
Julian Jacobi <julian.jacobi@wobcom.de>
2020-02-26 12:04:04 +0100

    add systemd unit

--- scripts/package_scripts/after_install.sh
+++ scripts/package_scripts/after_install.sh
@@ -6,34 +6,18 @@ chmod 700 /usr/bin/netbox-manage
 
 chown netbox:netbox /usr/bin/netbox-manage
 
+cp /opt/netbox/netbox.service /etc/systemd/system/netbox.service
+
+systemctl daemon-reload
+
 echo "Ensure PostgreSQL database is initialized"
 /usr/pgsql-9.6/bin/postgresql96-setup initdb
 
-echo "Ensure PostgreSQL and Redis services are started and enabled"
+echo "Ensure Netbox, PostgreSQL and Redis services are started and enabled"
 systemctl enable postgresql-9.6.service
 systemctl start postgresql-9.6.service
 systemctl enable redis.service
 systemctl start redis.service
+systemctl enable netbox.service
+systemctl start netbox.service
 
-# generate secret key
-if [[ ( "$(netbox-manage check 2>&1 | grep "SECRET_KEY" -c)" > 0 ) ]] ; then
-    echo "Generate secret key setting"
-    SECRET_KEY="$(/opt/netbox/venv/bin/python /opt/netbox/netbox/generate_secret_key.py)"
-    sed -i "s/SECRET_KEY = ''/SECRET_KEY = '${SECRET_KEY}'/g" /opt/netbox/netbox/netbox/configuration.py
-fi
-
-#generate database
-
-if [[ ( "$(su - postgres -c "psql -l -P format=unaligned" | grep -c "^netbox")" > 0 ) ]] ; then
-    echo "Netbox database already exist, please configure manually"
-else
-    echo "Creating database for netbox"
-    DB_NAME="netbox"
-    DB_USER="netbox"
-    DB_PASSWORD="$(/opt/netbox/venv/bin/python /opt/netbox/netbox/generate_secret_key.py)"
-    su - postgres -c "psql -c \"CREATE USER ${DB_USER} WITH PASSWORD '${DB_PASSWORD}';\""
-    su - postgres -c "psql -c \"createdb --owner ${DB_USER} ${DB_NAME}\""
-    sed -E -i "s/( +'USER':)[^#]*# PostgreSQL username.*/\1 '${DB_USER}'/g" /opt/netbox/netbox/netbox/configuration.py
-    sed -E -i "s/( +'NAME':)[^#]*# Database name.*/\1 '${DB_NAME}'/g" /opt/netbox/netbox/netbox/configuration.py
-    sed -E -i "s/( +'PASSWORD':)[^#]*# PostgreSQL password.*/\1 '${DB_PASSWORD}'/g" /opt/netbox/netbox/netbox/configuration.py
-fi
