53f774551c7d96104b40153a08052c3c683e5626
Julian Jacobi <julian.jacobi@wobcom.de>
2020-02-26 12:20:55 +0100

    add settings module to asgi

--- netbox/netbox/asgi.py
+++ netbox/netbox/asgi.py
@@ -1,8 +1,11 @@
+import os
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 import change.urls
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netbox.settings")
 application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(
         URLRouter(change.urls.websocket_urlpatterns)
