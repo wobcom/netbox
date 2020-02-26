a0415ee2f56911f7c2931093916c95570f133e70
Julian Jacobi <julian.jacobi@wobcom.de>
2020-02-26 11:29:20 +0100

    remove egg-link reloading

--- build_package.sh
+++ build_package.sh
@@ -13,9 +13,6 @@ venv/bin/python netbox/manage.py collectstatic
 echo ">> Copy final default configuration"
 cp netbox/netbox/configuration.example.py netbox/netbox/configuration.py
 echo ">> Rewrite venv egg-links"
-for LINK_FILE in venv/lib/python3.6/site-packages/*.egg-link ; do
-    sed -E -i "s/^.*\/venv(.*)/\/opt\/netbox\/venv\1/g" ${LINK_FILE}
-done
 
 LATEST_GIT_TAG=$(git describe --abbrev=0 --tags)
 VERSION=$(echo "${LATEST_GIT_TAG}" | tr - _ | sed -En "s/v(.*)/\\1/p" )
