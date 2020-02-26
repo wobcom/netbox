7acaf711a9a5a872da2d6a9c72509bedff6027eb
Julian Jacobi <julian.jacobi@wobcom.de>
2020-02-26 16:33:42 +0100

    fix asgi file

--- netbox/netbox/asgi.py
+++ netbox/netbox/asgi.py
@@ -1,11 +1,15 @@
 import os
 
+import django
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netbox.settings")
+django.setup()
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 import change.urls
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netbox.settings")
 application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(
         URLRouter(change.urls.websocket_urlpatterns)
