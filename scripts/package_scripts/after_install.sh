30b6c45082a5af87635d0a77109ff045ca4768c6
Julian Jacobi <julian.jacobi@wobcom.de>
2020-02-26 09:16:20 +0100

    add secret key generation on install

--- scripts/package_scripts/after_install.sh
+++ scripts/package_scripts/after_install.sh
@@ -5,3 +5,10 @@ cp /opt/netbox/scripts/package_scripts/netbox-manage /usr/bin/netbox-manage
 chmod 700 /usr/bin/netbox-manage
 
 chown netbox:netbox /usr/bin/netbox-manage
+
+# generate secret key
+if [[ ( "$(netbox-manage check 2>&1 | grep "SECRET_KEY" -c)" > 0 ) ]] ; then
+    echo "Generate secret key setting"
+    SECRET_KEY=$(/opt/netbox/venv/bin/python /opt/netbox/netbox/genrate_secret_key.py)
+    sed -i "s/SECRET_KEY = ''/SECRET_KEY = '${SECRET_KEY}'/g" /opt/netbox/netbox/netbox/configuration.py
+fi
