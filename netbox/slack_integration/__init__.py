4c10a466ef06ce6fc9c2de73a9f16a9fdca456ff
Julian Jacobi <julian.jacobi@wobcom.de>
2020-04-06 11:29:57 +0200

    fix typo in comment

--- netbox/slack_integration/__init__.py
+++ netbox/slack_integration/__init__.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ImproperlyConfigured
 
 default_app_config = 'slack_integration.apps.SlackConfig'
 
-# check that django-rq
+# check that django-rq is installed
 if settings.SLACK_ENABLED:
     try:
         import django_rq
