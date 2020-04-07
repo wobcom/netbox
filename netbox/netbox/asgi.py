697a1db1b630a471dd5f5a0ef3a29e400fd94115
Julian Jacobi <julian.jacobi@wobcom.de>
2020-04-07 10:21:36 +0200

    add `# noqa` to dirty module level import at asgi.py

--- netbox/netbox/asgi.py
+++ netbox/netbox/asgi.py
@@ -6,10 +6,10 @@ import django
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "netbox.settings")
 django.setup()
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack               # noqa
+from channels.routing import ProtocolTypeRouter, URLRouter  # noqa
 
-import change.urls
+import change.urls                                          # noqa
 
 
 application = ProtocolTypeRouter({
