36322cef8429520b9a25349628beca6feed4dc8d
Victor Nawothnig <Victor.Nawothnig@gmail.com>
2020-10-21 15:15:45 +0200

    Make LOGGING configurable

--- docker/configuration.py
+++ docker/configuration.py
@@ -145,7 +145,7 @@ EXEMPT_VIEW_PERMISSIONS = list(filter(None, os.environ.get('EXEMPT_VIEW_PERMISSI
 
 # Enable custom logging. Please see the Django documentation for detailed guidance on configuring custom logs:
 #   https://docs.djangoproject.com/en/1.11/topics/logging/
-LOGGING = {}
+LOGGING = json.loads(os.environ.get('LOGGING', '{}'))
 
 # Setting this to True will permit only authenticated users to access any part of NetBox. By default, anonymous users
 # are permitted to access most data in NetBox (excluding secrets) but not make any changes.
