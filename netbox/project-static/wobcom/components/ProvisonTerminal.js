20babbb2a1f7336a6f0e183c362c80f64b1ef8dc
Julian Jacobi <julian.jacobi@wobcom.de>
2020-12-22 12:22:20 +0100

    add expandability for terminal in porvision view

--- netbox/project-static/wobcom/components/ProvisonTerminal.js
+++ netbox/project-static/wobcom/components/ProvisonTerminal.js
@@ -2,14 +2,19 @@ import * as React from "react";
 import {Terminal} from "xterm";
 import {FitAddon} from "xterm-addon-fit";
 import {WebLinksAddon} from "xterm-addon-web-links";
+import {Button, Col, Panel} from "react-bootstrap";
 import PropTypes from "prop-types";
 
 export class ProvisionTerminal extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            fullscreen: false
+        }
         this.termRef = React.createRef()
         this.setupTerminal()
         this.startWebsocket(props.provisionId)
+        this.toggleFullscreen = this.toggleFullscreen.bind(this)
     }
 
     setupTerminal() {
@@ -26,6 +31,10 @@ export class ProvisionTerminal extends React.Component {
         this.terminalFit = new FitAddon()
         this.terminal.loadAddon(this.terminalFit)
         this.terminal.loadAddon(new WebLinksAddon())
+    }
+
+    toggleFullscreen() {
+        this.setState({fullscreen:!this.state.fullscreen});
     }
 
     startWebsocket(provisionId) {
@@ -50,12 +59,39 @@ export class ProvisionTerminal extends React.Component {
 
     componentDidMount() {
         this.terminal.open(this.termRef.current)
+    }
+
+    componentDidUpdate(prevProps, prevState, snapshot) {
         this.terminalFit.fit()
     }
 
     render() {
+
+        let fullscreenIcon = (<i className={'mdi mdi-arrow-expand'} title={'Expand'}/>)
+        let terminalSize = 8
+        if (this.state.fullscreen) {
+            fullscreenIcon = (<i className={'mdi mdi-arrow-collapse'} title={'Collapse'}/>)
+            terminalSize = 12
+        }
+
         return (
-            <div ref={this.termRef}/>
+            <Col md={terminalSize}>
+                <Panel>
+                    <Panel.Heading>
+                        <div className={'pull-right'}>
+                            <Button bsSize='xsmall' onClick={this.toggleFullscreen}>
+                                {fullscreenIcon}
+                            </Button>
+                        </div>
+                        <strong>
+                            Logs
+                        </strong>
+                    </Panel.Heading>
+                    <Panel.Body style={{padding: 0}}>
+                        <div ref={this.termRef}/>
+                    </Panel.Body>
+                </Panel>
+            </Col>
         )
     }
 }
